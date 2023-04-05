3eda19e6ccd77c184e14748f220a697faec5199d
井本拓伸 <inmthrnb@gmail.com>
2023-04-05 20:01:08 +0900

    コーディングを大幅に進めた

--- basic.js
+++ basic.js
@@ -1,44 +1,11 @@
-const yaohata = {};
-yaohata.ns = {
-	'h' : 'http://www.w3.org/1999/xhtml',
-	'mathml': 'http://www.w3.org/1998/Math/MathML',
-	's':'urn:yaohata-components-setting'
-};
-yaohata.nsResolver = function nsResolver(prefix) {
-	return yaohata.ns[prefix] || null;
-};
+window.defs = {};
 
-const parser = new DOMParser();
-const setting_loc = document.evaluate("//link[@rel='yaohata-components-setting']/@href",document,yaohata.nsResolver,XPathResult.STRING_TYPE,null).stringValue;
-let setting;
-let sefbase;
-function get(tag,key){
-	if(setting){
-		return setting.evaluate("/s:setting/s:"+tag+"/s:"+key+"/text()",setting,yaohata.nsResolver,XPathResult.STRING_TYPE,null).stringValue;
-	} else {
-		fetch(setting_loc)
-		.then((res)=>parser.parseFromString(res.text(),"application/xml"))
-		.then((dom)=>{
-			setting = dom;
-			return get(tag,key);
-		});
-	}
-}
-fetch(setting_loc)
-.then((res)=>res.text())
-.then((text)=>{
-	setting = parser.parseFromString(text,"application/xml");
-	sefbase = setting.evaluate("/s:setting/@sefbase",setting,yaohata.nsResolver,XPathResult.STRING_TYPE,null).stringValue;
-	console.log(sefbase);
-	customElements.define(TwitterButton.name,TwitterButton);
-	customElements.define("facebook-button",FacebookButton);
-	customElements.define("line-button",LineButton);
-	customElements.define("linkedin-button",LinkedInButton);
-	customElements.define("internal-quote",InternalQuote);
-	customElements.define("internal-fetch",InternalFetch);
-	customElements.define("internal-ogp",InternalOgp);
-})
-.catch((data)=>console.error(data));
+window.addEventListener("DOMContentLoaded",(e)=>{
+	customElements.define("window-def", Definition);
+	customElements.define("window-ref", Reference);
+	customElements.define("x-path", XPath);
+	customElements.define("console-log", ConsoleLog);
+});
 
 /*開発中*/
 class Version extends HTMLElement {
@@ -46,281 +13,175 @@ class Version extends HTMLElement {
 		this.replaceWith(document.createTextNode("1.1.0"));
 	}
 }
-
-class LinkURL extends HTMLElement {
-	constructor(){
-		super();
-		const a = document.createElement("a");
-		a.setAttribute("href",this.textContent);
-		a.appendChild(document.createTextNode(this.textContent));
-		for(let attr of this.getAttributeNames()) {
-			a.setAttributeNode(attr);
-		}
-		this.replaceWith(a);
-	}
-}
-customElements.define("link-url",LinkURL);
-
-/*meta name="**"の表示*/
-class MetaContent extends HTMLElement {
+class Definition extends HTMLElement {
 	constructor() {
 		super();
-		const elem = this;
-		const value = function(dom){
-			return dom.evaluate("//meta[@name='"+elem.getAttribute("name")+"']/@content",dom,yaohata.nsResolver,XPathResult.STRING_TYPE,null).stringValue;
-		}
-		const path = this.getAttribute("path");
-		let dom;
-		if(path) {
-			fetch(path)
-			.then((response)=>response.text())
-			.then((data)=>{
-				//テキストからHTMLをパースし、value関数で値を取得、テキストノードを作り、置き換える
-				this.replaceWith(document.createTextNode(value(parser.parseFromString(data,"text/html"))));
-			});
+		//name属性は必須、無ければエラーを投げる
+		const name = this.getAttribute("name");
+		if(!name) throw new Error("nameがない");
+
+		let target;
+		//ref属性があるなら参照する
+		if(this.hasAttribute("ref")) {
+			target = document.getElementById(this.getAttribute("ref"));
 		} else {
-			this.replaceWith(document.createTextNode(value(document)));
+			target = this;
 		}
-	}
-}
-customElements.define("meta-content",MetaContent);
-
-class TwitterButton extends HTMLElement {
-	constructor() {
-		super();
-
-		const arg = {
-			element:this,
-			name:"twitter-button",
-			sef:"sns.sef.json",
-			defaultTemplate:`<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" yc:data-url="{url}" data-show-count="false">Tweet</a><script async="" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>`,
-			params:{
-				"url":document.URL,
-				"title":document.querySelector('title').textContent,
-				"text":this.getAttribute("text")
-			}
-		 };
-		 exec(arg);
-	}
-	static name = "twitter-button";
-}
-
-
-class FacebookButton extends HTMLElement {
-
-	constructor() {
-		super();
-
-		const arg = {
-			element:this,
-			name:"facebook",
-			sef:"sns.sef.json",
-			defaultTemplate:`<div class="fb-share-button" yc:data-href="{url}" data-layout="button_count" data-size="small"><a target="_blank" yc:href="https://www.facebook.com/sharer/sharer.php?u={url}" class="fb-xfbml-parse-ignore">シェアする</a></div>`,
-			params:{
-				"url":document.URL,
-				"title":document.querySelector('title').textContent,
-				"text":this.getAttribute("text")
-			}
-		 }
-		 exec(arg);
-	}
-}
-
-
-class LineButton extends HTMLElement {
-	constructor() {
-		super();
-		
-		const arg = {
-			element:this,
-			name:"line",
-			sef:"sns.sef.json",
-			defaultTemplate:`<div class="line-it-button" data-lang="ja" data-type="share-a" data-env="REAL" yc:data-url="{url}" data-color="default" data-size="small" data-count="true" data-ver="3" style="display: none;"></div>
-			<script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>`,
-			params:{
-				"url":document.URL,
-				"title":document.querySelector('title').textContent,
-				"text":this.getAttribute("text")
-			}
-		 }
-		 exec(arg);
-	}
-}
-
-
-class LinkedInButton extends HTMLElement {
-	constructor() {
-		super();
-		
-		const arg = {
-			element:this,
-			name:"linkedin",
-			sef:"sns.sef.json",
-			defaultTemplate:`<script src="https://platform.linkedin.com/in.js" type="text/javascript"></script>
-			<script type="IN/Share" yc:data-url="{url}"></script><p><a href=""></a></p>
-			`,
-			params:{
-				"url":document.URL,
-				"title":document.querySelector('title').textContent,
-				"text":this.getAttribute("text")
-			}
-		 }
-		 exec(arg);
-	}
-}
-
-class FigureImage extends HTMLElement {
-	constructor(){
-		super();
-	}
-}
-//customElements.define("figure-image",FigureImage);
-
-/**/
-class InternalFetch extends HTMLElement {
-	constructor() {
-		super();
-
-		const elem = this;
-		fetch(this.getAttribute("path"))
-		.then((response)=>response.text())
-		.then((data)=>{
-			const arg = {
-				element:elem,
-				name:"internal-fetch",
-				sef:"fetch.sef.json",
-				defaultTemplate:`<yc:content/>`,
-				params:{
-					"path":elem.getAttribute("path"),
-					"target":elem.getAttribute("target"),
-					"template":elem.getAttribute("template"),
-					"document":parser.parseFromString(data,"text/html")
+		if(this.hasAttribute("value")) {
+			window.defs[name] = this.getAttribute("value");
+		} else if(this.hasAttribute("type")) {
+			const type = this.getAttribute("type");
+			
+			if(type=="string") {
+				window.defs[name] = target.textContent;
+			} else if(type=="dom") {
+				const f = document.createDocumentFragment();
+				let c = null;
+				while(c = target.firstChild) {
+					f.appendChild(c);
 				}
+				window.defs[name] = f;
+			} else if(type=="json") {
+				window.defs[name] = JSON.parse(target.textContent);
 			}
-			exec(arg);
+		} else {
+			window.defs[name] = target.textContent;
+		}
+		this.remove();
+	}
+}
+class Reference extends HTMLElement {
+	constructor() {
+		super();
+
+		const name = this.getAttribute("name");
+		if(!name) throw new Error("nameがない");
+
+		this.replaceWith(window.defs[name]);
+	}
+}
+
+class Framexs extends HTMLElement {
+
+}
+class SaxonTransfomer extends HTMLElement {
+	constructor() {
+		super();
+		const params = {};
+		this.querySelectorAll("param").forEach(param=>{
+			params[param.getAttribute("name")] = param.getAttribute("value");
+		});
+		if(this.getAttribute("html")) {
+
+		}
+		const options = {
+			stylesheetLocation: this.getAttribute("stylesheetLocation"),
+			//template要素はからのためcontentプロパティでDocumentFragmentを取得する
+			sourceNode: this.getAttribute("sourceNode"),
+			stylesheetParams: params,
+			destination: "document"
+		};
+		SaxonJS.transform(options,"async").then(d=>{
+
 		});
 	}
 }
-
-/*OGPを表示する*/
-class InternalOgp extends HTMLElement {
+/**
+ * 
+ */
+class ConsoleLog extends HTMLElement {
 	constructor() {
 		super();
-
-		const arg = {
-			element:this,
-			name:"internal-ogp",
-			sef:"ogp.sef.json",
-			defaultTemplate:`<div><img yc:src="{image}" width="64"/><a yc:href="{path}"><yc:title/></a><div><yc:description/></div></div>`,
-			params:{
-				"path":this.getAttribute("path")
-			}
-		}
-		exec(arg);
-	}
-}
-
-
-class InternalQuote extends HTMLElement {
-	constructor(){
-		super();
-		const elem = this;
-		fetch(this.getAttribute("path"))
-		.then((response)=>response.text())
-		.then((data)=>{
-			const arg = {
-				element:this,
-				name:"quote",
-				sef:"fetch.sef.json",
-				defaultTemplate:`<blockquote><yc:content/><p><cite><a yc:href="{path}"><yc:title/></a></cite></p></blockquote>`,
-				params:{
-					"path":this.getAttribute("path"),
-					"target":this.getAttribute("target"),
-					"document":parser.parseFromString(data,"text/html")
-				}
-			}
-			exec(arg);
-		});
-	}
-}
-
-
-class InternalSource extends HTMLElement {
-	constructor(){
-		super();
-
-		const arg = {
-			element:this,
-			name:"quote",
-			defaultTemplate:`<pre><yc:content/></pre>`,
-			params:{
-				"path":this.getAttribute("path"),
-				"from":this.getAttribute("from")||"",
-				"to":this.getAttribute("to")||""
-			}
-		}
-		exec(arg);
-	}
-}
-
-function exec(arg){
-	var s = new XMLSerializer();
-	var d = document;
-	
-	//文字列のHTMLをいったんtemplate要素を作って挿入し、DocumentFragmentを取り出す
-
-	let string = "<template xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:yc=\"urn:yaohata-components\">"+arg.defaultTemplate+"</template>";
-	let temp = parser.parseFromString(string,"application/xml");
-	let defaultTemplate = temp.documentElement.content;
-
-	//カスタム要素にtemplate属性がある場合を優先し、次にsettings要素での設定を見て、それでなければデフォルトのテンプレートを使う
-	let templateName = arg.element.getAttribute("template");
-	let template;
-	if(!templateName) {
-		console.log(get(arg.name,"template"));
-		let id = get(arg.name,"template");
-		if(id){
-			let t = document.getElementById(id).content;
-			let str = s.serializeToString(t);
-			let string = "<template xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:yc=\"urn:yaohata-components\">"+str+"</template>";
-			const dom = parser.parseFromString(string,"application/xml");
-			const f = dom.documentElement.content;
-			template = f;
+		let content;
+		if(this.hasAttribute("ref")) {
+			const target = document.getElementById(this.getAttribute("ref"));
+			if(target) content = target.textContent;
 		} else {
-			template = defaultTemplate;
+			content = this.textContent;
 		}
-	} else {
-		let t = document.getElementById(templateName).content;
-		let str = s.serializeToString(t);
-		let string = "<template xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:yc=\"urn:yaohata-components\">"+str+"</template>";
-		const dom = parser.parseFromString(string,"application/xml");
-		const f = dom.documentElement.content;
-		template = f;
-	}
-	const options = {
-		stylesheetLocation: sefbase+arg.sef,
-		//template要素はからのためcontentプロパティでDocumentFragmentを取得する
-		sourceNode: template,
-		stylesheetParams:arg.params,
-		destination:"document"
-	};
+		let val;
+		if(this.hasAttribute("type")) {
+			const type = this.getAttribute("type");
+			if(type=="string") {
+				val = content;
+			} else if(type=="dom") {
+				const f = document.createDocumentFragment();
+				let c = null;
+				while(c = this.firstChild) {
+					f.appendChild(c);
+				}
+				val = f;
+			} else if(type=="json") {
+				val = JSON.parse(content);
+			} else if(type=="xpath") {
 
-	SaxonJS.transform(options, "async")
-	.then(d=>{
-		console.log(d);
-		switch(arg.mode) {
-			case "replace":
-				arg.element.replaceWith(d.principalResult);
-				break;
-			case "shadow":
-				arg.element.attachShadow({mode:"open"})
-				//
-				.appendChild(d.principalResult);
-				break;
-			default:
-				arg.element.replaceWith(d.principalResult);
+				try {
+					val = SaxonJS.XPath.evaluate(content, document, {
+						params:{
+							"self":this,
+							"defs":window.defs
+						},
+						xpathDefaultNamespace: "http://www.w3.org/1999/xhtml"
+					});
+				} catch(e){
+					console.error(content + ":");
+					console.error(e);
+				}
+				
+			}
+		} else {
+			val = content;
 		}
-	 }).catch(v=>{
-		console.log(v);
-	 });
+		console.log(val);
+		this.remove();
+	}
+}
+/**
+ * SaxonJSのXPath機能を使う
+ */
+class XPath extends HTMLElement {
+	constructor() {
+		super();
+		let xpath;
+		//ref属性があるならwindow.defsから、その値の変数をXPathとする。
+		const ref = this.getAttribute("ref");
+		if(ref) {
+			if(typeof defs[ref] == 'string') {
+				xpath = window.defs[ref];
+			} else {
+				console.error(defs[ref]);
+				throw new Error("定義変数がstringでない。");
+			}
+		} else {
+			xpath = this.textContent;
+		}
+		//console.log(xpath);
+		const shadow = this.attachShadow({mode:"open"});
+		//documentをコンテキストに、自身をself、window.defsをdefsにバインドする
+		let result = SaxonJS.XPath.evaluate(xpath, document, {
+			params:{
+				"self":this,
+				"defs":window.defs
+			},
+			xpathDefaultNamespace: "http://www.w3.org/1999/xhtml"
+		});
+		//console.log(result);
+		
+		//result要素を作り,結果を付け加える
+		const resultnode = document.createElement("result");
+		if(result instanceof Node) {
+			const resultclone = result.cloneNode(true);
+			resultnode.append(resultclone);
+			//console.log(resultclone);
+		} else {
+			resultnode.append(result);
+		}
+		this.appendChild(resultnode);
+
+		shadow.append(result);
+		
+		
+		//self.replaceWith(result);
+
+	}
 }
